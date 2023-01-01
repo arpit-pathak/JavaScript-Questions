70f2bec517edee6e5ff7c72ddf5e1db79dfb2cd4
code-today <blogarpit21@gmail.com>
2023-01-01 23:39:50 +0530

    add some more ques and ans

--- Ques with solution/4_includes.js
+++ Ques with solution/4_includes.js
@@ -1,4 +1,5 @@
 // 4. Check if the string contains a word Script using includes() method.
-let paragraph = "Script is python javascript ksfdf fd fdsffi fs fjkfsl fisfs fsdfs fdsfsd";
+let paragraph =
+  "Script is python javascript ksfdf fd fdsffi fs fjkfsl fisfs fsdfs fdsfsd";
 let word = "Script";
 console.log(`It has include ${word} : ${paragraph.includes(word)}`);
